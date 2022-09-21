c33a80187a3a825fa609bf4af6df956842bc0aa0
jakkie <jakubkielbasiewicz@gmail.com>
2022-09-21 11:36:43 +0200

    Invaders rendering

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::error::Error;
+use rusty_audio::Audio;
+use crossterm::{terminal, ExecutableCommand};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::{Hide, Show};
+use std::time::Duration;
+use std::{io, thread};
+use std::sync::mpsc;
+use crossterm::event::{self, Event, KeyCode};
+use invaders::{frame, render};
+use invaders::frame::new_frame;
+
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
+
+    audio.play("startup");
+
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv(){
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    'gameloop: loop {
+        let curr_frame = new_frame();
+
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {
+                        
+                    }
+                }
+            }
+        }
+
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    drop(render_tx);
+    render_handle.join().unwrap();
+
+    audio.wait();
+    stdout.execute(Show)?;
+    Ok(())
+}
